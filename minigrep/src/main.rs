77a77cad59194be00b9aaa95fe87c6e1661ba714
Sergey Matyukevich <geomatsi@gmail.com>
2018-03-30 23:39:06 +0300

    minigrep: add file i/o example
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
+    // env example
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
@@ -8,4 +11,11 @@ fn main() {
     let fname = &args[2];
 
     println!("grep for {} in file {}", query, fname);
+
+    // file io example
+    let mut fh = File::open(fname).expect("file not found");
+    let mut text = String::new();
+
+    fh.read_to_string(&mut text).expect("failed to read from file");
+    println!("Text:\n{}", text);
 }
