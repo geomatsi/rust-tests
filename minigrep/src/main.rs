a541e8bad6ed5250bc65e9ca479dd6588615b36d
Sergey Matyukevich <geomatsi@gmail.com>
2018-03-31 23:39:07 +0300

    refactoring #4: error handling
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,28 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("config error: {}", err);
+        process::exit(1);
+    });
     println!("config '{:?}'", config);
 
-    let mut fh = File::open(config.fname).expect("file not found");
+    let mut fh = File::open(config.fname).unwrap_or_else(|err| {
+        println!("file open error: {}", err);
+        process::exit(1);
+    });
+
     let mut text = String::new();
-    fh.read_to_string(&mut text)
-        .expect("failed to read from file");
+    fh.read_to_string(&mut text).unwrap_or_else(|err| {
+        println!("read file error: {}", err);
+        process::exit(1);
+    });
 }
 
 #[derive(Debug)]
@@ -22,13 +32,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let q = &args[1];
         let f = &args[2];
 
-        Config {
+        Ok(Config {
             fname: f.clone(),
             query: q.clone(),
-        }
+        })
     }
 }
