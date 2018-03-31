224cdef48a95ef9789e14de6dc04f5a76f74f49f
Sergey Matyukevich <geomatsi@gmail.com>
2018-03-31 23:20:52 +0300

    refactoring #3: structure constructor
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     println!("config '{:?}'", config);
 
     let mut fh = File::open(config.fname).expect("file not found");
     let mut text = String::new();
-    fh.read_to_string(&mut text).expect("failed to read from file");
+    fh.read_to_string(&mut text)
+        .expect("failed to read from file");
 }
 
 #[derive(Debug)]
@@ -20,12 +21,14 @@ struct Config {
     fname: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let q = &args[1];
-    let f = &args[2];
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let q = &args[1];
+        let f = &args[2];
 
-    Config {
-        fname: f.clone(),
-        query: q.clone(),
+        Config {
+            fname: f.clone(),
+            query: q.clone(),
+        }
     }
 }
