57cbff5963947a64da328bb967faf1bc5c66bf6b
Sergey Matyukevich <geomatsi@gmail.com>
2018-03-31 23:04:14 +0300

    refactoring #1
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,19 +3,20 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    // env example
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let query = &args[1];
-    let fname = &args[2];
-
-    println!("grep for {} in file {}", query, fname);
-
-    // file io example
-    let mut fh = File::open(fname).expect("file not found");
-    let mut text = String::new();
-
-    fh.read_to_string(&mut text).expect("failed to read from file");
-    println!("Text:\n{}", text);
+    let (query, fname) = parse_config(&args);
+    println!("file '{}' query '{}'", query, fname);
 }
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let q = &args[1];
+    let f = &args[2];
+
+    return (f, q);
+}
+
+//let mut fh = File::open(fname).expect("file not found");
+//let mut text = String::new();
+//fh.read_to_string(&mut text).expect("failed to read from file");
