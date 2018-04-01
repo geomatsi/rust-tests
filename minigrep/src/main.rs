c6e7544d498ee4fe56605b22df13a251dd29be44
Sergey Matyukevich <geomatsi@gmail.com>
2018-04-01 11:58:42 +0300

    extract logic from main to run + error propagation
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,28 +1,32 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+use std::fs::File;
+use std::error::Error;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("config error: {}", err);
+        println!("make config error: {}", err);
         process::exit(1);
     });
     println!("config '{:?}'", config);
 
-    let mut fh = File::open(config.fname).unwrap_or_else(|err| {
-        println!("file open error: {}", err);
+    if let Err(e) = run(config) {
+        println!("run config error: {}", e);
         process::exit(1);
-    });
+    };
+}
 
+fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut fh = File::open(config.fname)?;
     let mut text = String::new();
-    fh.read_to_string(&mut text).unwrap_or_else(|err| {
-        println!("read file error: {}", err);
-        process::exit(1);
-    });
+
+    fh.read_to_string(&mut text)?;
+
+    Ok(())
 }
 
 #[derive(Debug)]
