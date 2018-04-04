f8c64f0269fce1527ac779660ef5bcf92e28b33a
Sergey Matyukevich <geomatsi@gmail.com>
2018-04-04 23:07:15 +0300

    cleanup
    - use stderr
    - remove debug messages
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,16 +7,14 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("make config error: {}", err);
+        eprintln!("make config error: {}", err);
         process::exit(1);
     });
-    println!("config '{}'", config);
 
     if let Err(e) = minigrep::run(config) {
-        println!("run config error: {}", e);
+        eprintln!("run config error: {}", e);
         process::exit(1);
     };
 }
