9b76cf9f6d7c352ef69d4ae78af0322c25ac771e
Sergey Matyukevich <geomatsi@gmail.com>
2018-04-08 16:50:05 +0300

    closures: structures and Fn trait
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- scratch-board/src/bin/test-closures.rs
+++ scratch-board/src/bin/test-closures.rs
@@ -5,22 +5,48 @@ use rand::Rng;
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T> where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T> where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
 fn sim_generate_workout(intensity: u32) {
     let rand = rand::thread_rng().gen_range(1, 8);
 
-    let opt_res = |num: u32| -> u32 {
+    let mut opt_res = Cacher::new(|num: u32| -> u32 {
         println!("slow calculation...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity >= 10 {
-        println!("Run {} km", opt_res(intensity));
+        println!("Run {} km", opt_res.value(intensity));
     } else {
         if rand == 3 {
             println!("Time to recover, take a break today!");
         } else {
-            println!("Recovery run {} km", opt_res(intensity));
+            println!("Recovery run {} km", opt_res.value(intensity));
         }
     }
 }
