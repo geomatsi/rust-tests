fe59d3250a685c37e79a3ea92431fc4f7cf8b3d7
Sergey Matyukevich <geomatsi@gmail.com>
2018-04-08 14:03:34 +0300

    closures: add example
    no explicit type annotations
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- scratch-board/src/bin/test-closures.rs
+++ scratch-board/src/bin/test-closures.rs
@@ -1,22 +1,22 @@
 use std::thread;
 use std::time::Duration;
 
-fn sim_long_calc(intensity: u32) -> u32 {
-    println!("slow calculation...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
 
 fn sim_generate_workout(intensity: u32, rand: u32) {
-    let calc_res = sim_long_calc(intensity);
+
+    let opt_res = |num| {
+        println!("slow calculation...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
 
     if intensity >= 10 {
-        println!("Run {} km", calc_res);
+        println!("Run {} km", opt_res(intensity));
     } else {
         if rand == 3 {
             println!("Time to recover, take a break today!");
         } else {
-            println!("Recovery run {} km", calc_res);
+            println!("Recovery run {} km", opt_res(intensity));
         }
     }
 }
