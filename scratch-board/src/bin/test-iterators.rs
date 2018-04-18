9aa67152f19a6aac9d3d0eb3c9b8629b5d687565
Sergey Matyukevich <geomatsi@gmail.com>
2018-04-18 23:38:40 +0300

    add example: implement iterator for custom type
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- scratch-board/src/bin/test-iterators.rs
+++ scratch-board/src/bin/test-iterators.rs
@@ -15,6 +15,43 @@ impl clone::Clone for Shoe {
     }
 }
 
+#[derive(Debug)]
+struct Counter {
+    count: u32,
+    max: u32,
+}
+
+impl Counter {
+    fn new(m: u32) -> Counter {
+        Counter { count: 0, max: m }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count <= self.max {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+impl clone::Clone for Counter {
+    fn clone(&self) -> Counter {
+        Counter {
+            count: self.count,
+            max: self.max,
+        }
+    }
+}
+
+//
+
 fn shoes_in_my_size(shoes: &Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
     shoes
         .into_iter()
@@ -24,6 +61,8 @@ fn shoes_in_my_size(shoes: &Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
 }
 
 fn main() {
+    // Example 1
+
     let shoes = vec![
         Shoe {
             size: 10,
@@ -60,4 +99,28 @@ fn main() {
     for s in shoes_list {
         println!("  model: {}, size: {}", s.model, s.size);
     }
+
+    println!("Shoe list {:?}", shoes);
+
+    // Example 2
+
+    let c = Counter::new(5);
+
+    println!("c = {:?}", c);
+
+    for s in c.clone() {
+        println!("counter values: {}", s);
+    }
+
+    for s in c.clone().map(|x| x*x).filter(|x| (*x < 15)) {
+        println!("counter filtered: {}", s);
+    }
+
+    println!("c = {:?}", c);
+
+    for s in c {
+        println!("counter: {}", s);
+    }
+
+    // no more can use c: it has been moved into iterator
 }
