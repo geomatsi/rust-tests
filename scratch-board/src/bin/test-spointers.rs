0b9d76da874f51065645f4c0f5cdef8c3700cbae
Sergey Matyukevich <geomatsi@gmail.com>
2018-05-17 23:32:17 +0300

    box: recursive data structures
    Cons list and its head.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- scratch-board/src/bin/test-spointers.rs
+++ scratch-board/src/bin/test-spointers.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("b = {}", b);
 
     //
- 
+
     println!("run tests: cargo test --bin test-iterators");
 }
 
@@ -23,4 +23,31 @@ fn f_test_box() {
         assert_eq!(*Box::into_raw(v1), 5u32);
         assert_eq!(*Box::into_raw(v2), "hello");
     }
+}
+
+//
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn cons_head(v: &List) -> Option<i32> {
+    match *v {
+        Cons(head, _) => Some(head),
+        Nil => None,
+    }
+}
+
+#[test]
+fn f_test_cons() {
+    let list1 = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let list2 = Nil;
+
+    assert_eq!(cons_head(&list1), Some(1));
+    assert_eq!(cons_head(&list1), Some(1));
+
+    assert_eq!(cons_head(&list2), None);
 }
