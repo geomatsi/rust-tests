ce18dfbafe9a113a0739fec41075b6fa3bf01647
Sergey Matyukevich <geomatsi@gmail.com>
2018-06-14 23:49:34 +0300

    thread: move structure into thread
    Note that more complicated structure shall implement
    Copy trait in order to be 'moveable' to thread.
    
    For instance, similar structure with String field can't be handled
    in the same way since String does not implement Copy trait.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- scratch-board/src/bin/test-concurrency.rs
+++ scratch-board/src/bin/test-concurrency.rs
@@ -20,3 +20,37 @@ fn f_test_thread_closure_v1() {
 
     assert_eq!(a, 5);
 }
+
+//
+
+struct Env {
+    key: u32,
+}
+
+impl Env {
+    fn new(k: u32) -> Env {
+        Env { key: k }
+    }
+}
+
+impl Copy for Env {}
+
+impl Clone for Env {
+    fn clone(&self) -> Env {
+        Env { key: self.key }
+    }
+}
+
+#[test]
+fn f_test_thread_closure_v2() {
+    let mut v = Env::new(10);
+
+    let handle = thread::spawn(move || {
+        v.key *= 2;
+        assert_eq!(v.key, 20);
+    });
+
+    handle.join().unwrap();
+
+    assert_eq!(v.key, 10);
+}
