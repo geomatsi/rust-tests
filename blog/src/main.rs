4404b4a6a2a861b451001cf251ef74cabc452123
Sergey Matyukevich <geomatsi@gmail.com>
2018-07-09 23:30:18 +0300

    state pattern using traits
    Example from rust book and tests for it.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ blog/src/main.rs
@@ -0,0 +1,51 @@
+extern crate blog;
+
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("Hello World");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("Hello World", post.content());
+    println!("Content: {}", post.content());
+}
+
+#[test]
+fn f_test_v1() {
+    let post = Post::new();
+
+    assert_eq!("", post.content());
+}
+
+#[test]
+fn f_test_v2() {
+    let mut post = Post::new();
+
+    post.add_text("Hello World");
+    assert_eq!("", post.content());
+}
+
+#[test]
+fn f_test_v3() {
+    let mut post = Post::new();
+
+    post.add_text("Hello World");
+    post.request_review();
+    assert_eq!("", post.content());
+}
+
+#[test]
+fn f_test_v4() {
+    let mut post = Post::new();
+
+    post.add_text("Hello World");
+    post.request_review();
+    post.approve();
+    assert_eq!("Hello World", post.content());
+}
