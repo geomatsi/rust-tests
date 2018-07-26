f9162a280f10754b9337ce1099df7ecadddd7fe3
Sergey Matyukevich <geomatsi@gmail.com>
2018-07-26 23:54:17 +0300

    more match examples
    - destructuring enums, structs
    - references
    - ignoring parts
    - guards
    - @ bindings
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- scratch-board/src/bin/test-match.rs
+++ scratch-board/src/bin/test-match.rs
@@ -87,7 +87,7 @@ fn main() {
         }
     }
 
-    //
+    // destructuring structs I
 
     let p = Point { x: 0, y: 7 };
     let Point { x: a1, y: b1 } = p;
@@ -98,7 +98,7 @@ fn main() {
     assert_eq!(0, x);
     assert_eq!(7, y);
 
-    //
+    // destructuring structs II
 
     let mut polygon = vec![
         Point { x: 0, y: 1 },
@@ -115,7 +115,7 @@ fn main() {
         }
     }
 
-    //
+    // destructuring enums
 
     let mut batch = vec![
         Command::Turn(45),
@@ -135,5 +135,85 @@ fn main() {
         }
     }
 
-    //
+    // destructuring references
+
+    let points = vec![
+        Point { x: 0, y: 1 },
+        Point { x: 1, y: 0 },
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 1 },
+    ];
+
+    let value: i32 = points.iter().map(|&Point { x, y }| x * x + y * y).sum();
+    println!("sum of squares: {}", value);
+
+    // ignoring values in patterns
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (a, _, c, _, e) => println!("Some numbers: {}, {}, {}", a, c, e),
+    }
+
+    // ignoring remaining parts
+
+    let origin = Point { x: 0, y: 0 };
+
+    match origin {
+        Point { x, .. } => println!("x is {}", x),
+    }
+
+    let seq = (1, 2, 3, 4, 5, 6);
+
+    match seq {
+        (fst, .., lst) => println!("first: {}, last: {}", fst, lst),
+    }
+
+    // creating references in patterns
+
+    let cat_name = Some(String::from("Barsik"));
+
+    match cat_name {
+        Some(ref name) => println!("Cat: {}", name),
+        None => println!("No cats..."),
+    }
+
+    // NB: 'cat_name' ownership was not taken by 'name' !
+    println!("Cat: {:?}", cat_name);
+
+    // creating mutable references in patterns
+
+    let mut cat_name = Some(String::from("Barsik"));
+
+    match cat_name {
+        Some(ref mut name) => *name = String::from("Murka"),
+        None => println!("No cats..."),
+    }
+
+    println!("Cat: {:?}", cat_name);
+
+    // guards
+
+    let mut seq = vec![1, 3, 5, 6, 7];
+
+    while let Some(n) = seq.pop() {
+        match n {
+            1 => println!("1"),
+            2 | 3 => println!("2 or 3"),
+            n if n > 3 && n < 7 => println!("(3, 7)"),
+            _ => println!("default: {}", n),
+        }
+    }
+
+    // @ binding
+
+    let mut seq = vec![1, 3, 5, 6, 7];
+
+    while let Some(n) = seq.pop() {
+        match n {
+            1...3 => println!("some value in range [1, 3]"),
+            val @ 4...6 => println!("known value in another range: {}", val),
+            val @ _ => println!("known default value:{}", val),
+        }
+    }
 }
