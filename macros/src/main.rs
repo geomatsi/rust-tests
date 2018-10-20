d14a3193a4c3fa1e2c716a48d9eb5b56393332b2
Sergey Matyukevich <geomatsi@gmail.com>
2018-10-20 23:20:18 +0300

    more flexible sum! macro: types
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -44,3 +44,28 @@ fn f_test_m2() {
     assert_eq!(bar(3), 12);
     assert_eq!(foo({ 2 + 2 }), bar(2));
 }
+
+macro_rules! sum2 {
+    ($x:expr) => (
+        $x
+    );
+    ($x:expr, $($y:expr), +) => (
+        {
+            let mut s = $x;
+            $ (
+                s += $y;
+            )+
+            s
+        }
+    )
+}
+
+#[test]
+fn f_test_m3() {
+    assert_eq!(sum2![1], 1);
+    assert_eq!(sum2![1u32, 2u32], 3u32);
+    assert_eq!(sum2![-1i32, 2i32], 1i32);
+    assert_eq!(sum2![1.0], 1.0);
+    assert_eq!(sum2![1, 2], 3);
+    assert_eq!(sum2![1.0, 2.0, { 1.0 + 1.0 }], 5.0);
+}
