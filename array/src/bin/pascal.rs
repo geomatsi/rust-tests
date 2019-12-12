bb5e435ff23701591ce956330d9b9a7109e6eb21
Sergey Matyukevich <geomatsi@gmail.com>
2019-12-12 23:33:45 +0300

    array example 12
    Get specified row of Pascal triangle.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- array/src/bin/pascal.rs
+++ array/src/bin/pascal.rs
@@ -33,6 +33,26 @@ fn generate(num_rows: i32) -> Vec<Vec<i32>> {
     res
 }
 
+#[allow(dead_code)]
+pub fn get_row(row_index: i32) -> Vec<i32> {
+    let mut res: Vec<i32> = vec![0; (row_index as usize) + 1];
+
+    res[0] = 1;
+
+    for n in 0..(row_index as usize + 2) {
+        let mut a: i32 = 0;
+        let mut b: i32;
+
+        for i in 0..n {
+            b = a;
+            a = res[i];
+            res[i] = res[i] + b;
+        }
+    }
+
+    res
+}
+
 /* tests */
 
 #[test]
@@ -73,4 +93,15 @@ fn pascal_test() {
             vec![1, 6, 15, 20, 15, 6, 1]
         ]
     );
+}
+
+#[test]
+fn pascal1_test() {
+    assert_eq!(get_row(0), vec![1]);
+    assert_eq!(get_row(1), vec![1, 1]);
+    assert_eq!(get_row(2), vec![1, 2, 1]);
+    assert_eq!(get_row(3), vec![1, 3, 3, 1]);
+    assert_eq!(get_row(4), vec![1, 4, 6, 4, 1]);
+    assert_eq!(get_row(5), vec![1, 5, 10, 10, 5, 1]);
+    assert_eq!(get_row(6), vec![1, 6, 15, 20, 15, 6, 1]);
 }
