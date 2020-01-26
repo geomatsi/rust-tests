ed9cfdc07a884bd4f5cbb528a05d197813ce9ed3
Sergey Matyukevich <geomatsi@gmail.com>
2020-01-27 00:45:49 +0300

    crypto: rc4: optimize calculations
    Calculate SBox in RC4 constructor rather than do it every time on message
    encryption or decription. In this case both IV and KEY as well as KBox
    are not needed since they are just inputs for SBox. So do not include
    them into RC4 structure. However size params are needed as long as we
    use stream_cipher traits. So add two PhantomData internal handlers.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- crypto/src/rc4.rs
+++ crypto/src/rc4.rs
@@ -1,13 +1,16 @@
+use core::marker::PhantomData;
+use stream_cipher::generic_array::typenum::U256;
 use stream_cipher::generic_array::ArrayLength;
 use stream_cipher::generic_array::GenericArray;
 use stream_cipher::NewStreamCipher;
 use stream_cipher::StreamCipher;
 
-// RC4
+/* RC4 */
 
 pub struct RC4<M: ArrayLength<u8>, N: ArrayLength<u8>> {
-    key: GenericArray<u8, M>,
-    iv: GenericArray<u8, N>,
+    _k: PhantomData<GenericArray<u8, M>>,
+    _v: PhantomData<GenericArray<u8, N>>,
+    sbox: GenericArray<u8, U256>,
 }
 
 impl<M, N> NewStreamCipher for RC4<M, N>
@@ -19,9 +22,29 @@ where
     type NonceSize = N;
 
     fn new(key: &GenericArray<u8, Self::KeySize>, iv: &GenericArray<u8, Self::NonceSize>) -> Self {
+        let mut sbox: Vec<u8> = (0..=255).map(|i| i).collect();
+        let mut kbox: Vec<u8> = vec![];
+        let mut j: u8 = 0;
+        let mut t: u8;
+
+        while kbox.len() < 256 {
+            kbox.extend_from_slice(iv.as_slice());
+            kbox.extend_from_slice(key.as_slice());
+        }
+
+        for i in 0..=255 {
+            j = j.wrapping_add(sbox[i]);
+            j = j.wrapping_add(kbox[i]);
+
+            t = sbox[i];
+            sbox[i] = sbox[j as usize];
+            sbox[j as usize] = t;
+        }
+
         RC4 {
-            key: key.clone(),
-            iv: iv.clone(),
+            sbox: *GenericArray::from_slice(&sbox[0..256]),
+            _k: PhantomData,
+            _v: PhantomData,
         }
     }
 }
@@ -32,42 +55,19 @@ where
     N: ArrayLength<u8>,
 {
     fn encrypt(&mut self, data: &mut [u8]) {
-        let mut sbox: Vec<u8> = (0..=255).map(|i| i).collect();
-        let mut kbox: Vec<u8> = vec![];
-        let mut i: u8;
-        let mut j: u8;
+        let mut sbox: Vec<u8> = self.sbox.to_vec();
+        let mut i: u8 = 0;
+        let mut j: u8 = 0;
         let mut t: u8;
 
-        kbox.extend_from_slice(self.iv.as_slice());
-        kbox.extend_from_slice(self.key.as_slice());
-        j = 0;
-
-        for i in 0..=255 {
-            // j = (j + S_i + K_i) mod 256
-            j = j.wrapping_add(sbox[i]);
-            j = j.wrapping_add(kbox[i % kbox.len()]);
-
-            // swap S_i and S_j
-            t = sbox[i];
-            sbox[i] = sbox[j as usize];
-            sbox[j as usize] = t;
-        }
-
-        i = 0;
-        j = 0;
-
         for e in data.iter_mut() {
-            // i = (i + 1) mod 256
             i = i.wrapping_add(1);
             j = j.wrapping_add(sbox[i as usize]);
 
-            // swap S_i and S_j
             t = sbox[i as usize];
             sbox[i as usize] = sbox[j as usize];
             sbox[j as usize] = t;
 
-            // k = (S_i + S_j) mod 256
-            // R = S_k
             t = sbox[sbox[i as usize].wrapping_add(sbox[j as usize]) as usize];
             *e ^= t;
         }
