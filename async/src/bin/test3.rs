fa73dcdac176e1aa3a7df88e34541c627e3eb555
Sergey Matyukevich <geomatsi@gmail.com>
2020-02-02 23:23:15 +0300

    test3: add select example
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- async/src/bin/test3.rs
+++ async/src/bin/test3.rs
@@ -1,5 +1,5 @@
 use {
-    futures::{executor::block_on, join},
+    futures::{executor::block_on, join, pin_mut, select, FutureExt},
     std::{
         future::Future,
         pin::Pin,
@@ -70,15 +70,71 @@ impl FiboFuture {
 }
 
 fn main() {
+    // block_on
+
     for i in 1..10 {
         let f = FiboFuture::create(i);
         let v = block_on(f);
         println!("fib({}) = {}", i, v);
     }
 
+    // join
+
     let f1 = FiboFuture::create(40);
     let f2 = FiboFuture::create(10);
 
     let (a, b) = block_on(async { join!(f1, f2) });
-    println!("fib(100) = {}, fib(10) = {}", a, b);
+    println!("fib(40) = {}, fib(10) = {}", a, b);
+
+    // select: wait in loop for all completions
+
+    let f3 = FiboFuture::create(70).fuse();
+    let f4 = FiboFuture::create(50).fuse();
+    let f5 = FiboFuture::create(30).fuse();
+
+    pin_mut!(f3, f4, f5);
+
+    block_on(async {
+        loop {
+            select! {
+                n = f3 => println!("Fib(70) = {}", n),
+                n = f4 => println!("Fib(50) = {}", n),
+                n = f5 => println!("Fib(30) = {}", n),
+                default => println!("not yet..."),
+                complete => break,
+            }
+        }
+    });
+
+    // select: wait for the next completion
+
+    let f6 = FiboFuture::create(71).fuse();
+    let f7 = FiboFuture::create(51).fuse();
+    let f8 = FiboFuture::create(31).fuse();
+
+    pin_mut!(f6, f7, f8);
+
+    block_on(async {
+        select! {
+            n = f6 => println!("Fib(71) = {}", n),
+            n = f7 => println!("Fib(51) = {}", n),
+            n = f8 => println!("Fib(31) = {}", n),
+        }
+    });
+
+    block_on(async {
+        select! {
+            n = f6 => println!("Fib(71) = {}", n),
+            n = f7 => println!("Fib(51) = {}", n),
+            n = f8 => println!("Fib(31) = {}", n),
+        }
+    });
+
+    block_on(async {
+        select! {
+            n = f6 => println!("Fib(71) = {}", n),
+            n = f7 => println!("Fib(51) = {}", n),
+            n = f8 => println!("Fib(31) = {}", n),
+        }
+    });
 }
